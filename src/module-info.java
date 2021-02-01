ab27db89f87e3d816278e5580214b1ee8aa041b5
lukasz.wypych <lukasz.wypych@STREAMSOFT-0064.streamsoft.pl>
2021-02-01 11:13:49 +0100

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module currencyexchange {
+}
---
+++ src/pl/streamsoft/currencyexchange/Main.java
@@ -0,0 +1,17 @@
+package pl.streamsoft.currencyexchange;
+
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+
+private static final String GET_URL = "http://api.nbp.pl/api/exchangerates/rates/A/";
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
